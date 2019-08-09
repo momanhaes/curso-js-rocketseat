9dbc5bf73dc55c9d22fa385d49e7413b5d46f25e
Matheus Manhães <momanhaes@gmail.com>
2019-08-09 14:40:45 -0300

    Adiciona resolução do exercício 5 do módulo 1

--- Exercícios/Exercícios Módulo 1/exercicios1.js
+++ Exercícios/Exercícios Módulo 1/exercicios1.js
@@ -103,6 +103,34 @@
 // experiencia(anosEstudo);
 // //Fim Exercício 4
 
-//Exercício 5
+// //Exercício 5
+function exibeHabilidades(usuarios) {
+    for (usuario of usuarios) {
+        console.log(
+            "O " +
+            usuario.nome +
+            " possui as habilidades: " +
+            usuario.habilidades.join(", ")
+        );
+    }
+}
 
-//Fim Exercício 5
+var usuarios = [
+    {
+        nome: "Diego",
+        habilidades: ["Javascript", "ReactJS", "Redux"]
+    },
+    {
+        nome: "Gabriel",
+        habilidades: ["VueJS", "Ruby on Rails", "Elixir"]
+    }
+];
+
+exibeHabilidades(usuarios);
+// }
+
+//O Diego possui as habilidades: Javascript, ReactJS, Redux
+//O Gabriel possui as habilidades: VueJS, Ruby on Rails, Elixir
+
+
+// //Fim Exercício 5
